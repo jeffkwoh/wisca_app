3e88cb0016a4afb801259fc55b3feb1d7e94aea0
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2019-01-03 18:32:39 +0800

    Add button

--- app/src/main/java/ritwik/graphapp/AppointmentsFragment.java
+++ app/src/main/java/ritwik/graphapp/AppointmentsFragment.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,14 +30,28 @@ public class AppointmentsFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         // Mockdata for recycler view
-        List<Appointment> appointmentList = new ArrayList<>(); // TODO: List should be sorted
+        final List<Appointment> appointmentList = new ArrayList<>(); // TODO: List should be sorted
         appointmentList.add(new Appointment(0, "11 Oct 2018", "99kg", "Finger stuck in fidget spinner"));
         appointmentList.add(new Appointment(0, "11 Sep 2018", "20kg", "Both arms broken"));
 
         // Set adapter
-        AppointmentAdapter appointmentAdapter = new AppointmentAdapter(getContext(), appointmentList);
+        final AppointmentAdapter appointmentAdapter = new AppointmentAdapter(getContext(), appointmentList);
         recyclerView.setAdapter(appointmentAdapter);
 
+        // Set button listener
+        Button addAppointmentButton = rootView.findViewById(R.id.buttonAddAppointment);
+        addAppointmentButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Dummy method to test button.
+                appointmentList.add(new Appointment(0, "231", "sddsdq", "dofijsoifsd"));
+                appointmentAdapter.notifyDataSetChanged();
+            }
+        });
+
         return rootView;
+    }
+
+    public void onAddItem(View v) {
+        appointmentList.add(new Appointment(0, "231", "sddsdq", "dofijsoifsd"));
     }
 }
