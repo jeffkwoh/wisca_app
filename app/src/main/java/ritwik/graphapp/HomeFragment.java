820ef7d8ae85eed0b8507ff169c2b892feab8fbd
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2019-01-05 17:13:14 +0800

    Dirty Commit: Working NFC intent pass to fragment

--- app/src/main/java/ritwik/graphapp/HomeFragment.java
+++ app/src/main/java/ritwik/graphapp/HomeFragment.java
@@ -3,9 +3,12 @@ package ritwik.graphapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class HomeFragment extends Fragment {
@@ -13,6 +16,22 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Set Button Handler
+        Button buttonNfcRead = (Button) view.findViewById(R.id.buttonNfcRead);
+        final TextView textViewNfcReadData = (TextView) view.findViewById(R.id.textViewNfcReadData);
+        buttonNfcRead.setOnClickListener((View v) -> {
+            textViewNfcReadData.setText("READING");
+        });
+
+        String nfcString = getArguments() == null ?
+                null : getArguments().getString("nfcData");
+        Log.d("nfcString", nfcString == null ? "null" : nfcString);
+        if (nfcString != null) {
+            textViewNfcReadData.setText(nfcString);
+        }
+
+        return view;
     }
 }
--- app/src/main/java/ritwik/graphapp/WeightLog.java
+++ app/src/main/java/ritwik/graphapp/WeightLog.java
@@ -1,14 +1,23 @@
 package ritwik.graphapp;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class WeightLog extends AppCompatActivity /*implements OnChartGestureListener, OnChartValueSelectedListener*/ {
+import ritwik.graphapp.NfcUtils.MifareUltralightTagTester;
 
+public class WeightLog extends AppCompatActivity /*implements OnChartGestureListener, OnChartValueSelectedListener*/ {
+    NfcAdapter mNfcAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,58 @@ public class WeightLog extends AppCompatActivity /*implements OnChartGestureList
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeFragment()).commit();
         }
+    }
+
+    // TODO: Create NFC Interface for encapsulation and to ensure activity implements NFC methods
+    public NfcAdapter getNfcAdapter() {
+        if (mNfcAdapter == null) {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        }
+        return mNfcAdapter;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+            // Build Fragment
+            Fragment homeFragment = new HomeFragment();
+            Bundle mBundle =  new Bundle();
+            mBundle.putString("nfcData", "passed data");
+            homeFragment.setArguments(mBundle);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, homeFragment).commit();
+
+//            Parcelable[] rawMessages =
+//                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//            if (rawMessages != null) {
+//                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+//                for (int i = 0; i < rawMessages.length; i++) {
+//                    messages[i] = (NdefMessage) rawMessages[i];
+//                }
+//                // Process the messages array.
+//
+//            }
+        }
+    }
+
+    public String readNfc(){
+        MifareUltralightTagTester tester = new MifareUltralightTagTester();
+        NfcAdapter nfcAdapter = getNfcAdapter();
+        return null;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -52,5 +113,7 @@ public class WeightLog extends AppCompatActivity /*implements OnChartGestureList
                 return true;
             }
     };
+
+
 
 }
